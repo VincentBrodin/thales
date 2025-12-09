f0ea5710cd09769f2970880c4b3da631d8499aeb
Vincent Brodin <vincent.brodin21@gmail.com>
2025-12-09 17:18:26 +0100

    added flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.cargo
+          pkgs.clippy
+          pkgs.rustc
+          pkgs.rustfmt
+          pkgs.rust-analyzer
+          pkgs.openssl
+          pkgs.nodePackages.vscode-json-languageserver
+        ];
+
+        shellHook = ''
+          exec zsh
+          export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";          
+          alias test="cargo test --all-features -- --no-capture";          
+        '';
+      };
+    };
+}
